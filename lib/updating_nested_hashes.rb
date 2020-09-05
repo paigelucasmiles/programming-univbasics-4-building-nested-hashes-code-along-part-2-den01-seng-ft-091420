c464333c73e47e7e7362e7cab965889880baad52
Paige Miles <paige@lucas-miles.com>
2020-09-05 23:36:31 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -48,6 +48,9 @@ def add_characters
          ]
       }
    }
+end
+
+
    # Write code below that does the following programmatically:
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
